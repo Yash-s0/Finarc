cb6937244bb102c572d8ce460e76b6e6c415d919
Yash Sharma <yash.sharma@stackera.io>
2026-08-04 14:36:07 +0530

    Update version to 1.1.41+61 and increment target SDK version
    - Incremented version number in pubspec.yaml from 1.1.40+60 to 1.1.41+61.
    - Updated target SDK version in build.gradle.kts from 35 to 36 for improved compatibility and performance.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -25,7 +25,7 @@ android {
     defaultConfig {
         applicationId = "com.yashsharma.finarc"
         minSdk = flutter.minSdkVersion
-        targetSdk = 35
+        targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
